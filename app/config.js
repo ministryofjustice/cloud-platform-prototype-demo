a80a5142752bea9e0b1ebe702ad1a0eff46b326a
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2022-05-23 18:24:32 +0100

    Change js to reflect in the branch

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Prototype deployed from branch-testing',
 
   // Default port that prototype runs on
   port: '3000',
